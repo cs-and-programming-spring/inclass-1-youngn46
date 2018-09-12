24eeb3c79f9b09020e51f5636170e37cc936fa23
takeshwari <kamalt4@students.rowan.edu>
2018-09-12 10:59:24 -0400

    area of circle program

--- Source.cpp
+++ Source.cpp
@@ -1,9 +1,40 @@
+
+
+
+/***********************************************
+* Name: Takeshwari Kamal
+* Email: kamalt4@students.rowan.edu
+***********************************************
+* Purpose:
+*  This program calculates area of circle
+***********************************************/
+
 #include <iostream>
 
 using namespace std; 
 
+const  float PI = 3.14159;
+
 int main()
 {
-	cout << "Hello, world!" << endl;
+
+	float radius, area, circumference;
+	
+	//Display purpose of the program
+	cout << " ******************************************"<<endl;
+	cout << " This program takes in radius to compute area of circle" <<endl;
+	cout << " ******************************************\n\n";
+	
+	//Get user input
+	cout << "Please input radius: ";
+	cin >> radius;
+	
+	//Compute area to PI*radius*radius
+	area = PI* radius *radius; 
+	
+	//Display the area 
+	cout << " The area is = " << area << "\n\n";
+	cout << "Program has ended"<<endl;
+	
 	return 0;
 }
